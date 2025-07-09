401f3911af0079f21b696b32e7fa8d5d8213ec38
IlyaKIS1 <ilyuhakostin@gmail.com>
2025-07-09 02:59:18 -0400

    created postgres host

--- backend/datasource.js
+++ backend/datasource.js
@@ -1,10 +1,11 @@
 import pg from "pg";
 
 export const client = new pg.Client({
-  host: "localhost",
-  user: "postgres",
-  port: 5432,
-  database: "postgres",
+  host: process.env.POSTGRES_HOST || "localhost",
+  user: process.env.POSTGRES_USER || "postgres",
+  password: process.env.POSTGRES_PASSWORD || "postgres",
+  port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432,
+  database: process.env.POSTGRES_DB || "boardshowdown",
 });
 
-await client.connect(); 
+await client.connect();
