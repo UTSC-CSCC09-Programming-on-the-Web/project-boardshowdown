eaf11ea24010cf70bebe1173a80926046b1b851f
IlyaKIS1 <ilyuhakostin@gmail.com>
2025-07-11 17:08:14 -0400

    check build modification

--- frontend/src/app/whiteboard/whiteboard.ts
+++ frontend/src/app/whiteboard/whiteboard.ts
@@ -19,7 +19,7 @@ import { environment } from '../../environments/environment';
 import { WebsocketProvider } from 'y-websocket';
 import { QuestionService, Question, CheckSolutionResult } from '../services/question.service';
 
-//deploy 1x
+//deploy 2x
 @Component({
   selector: 'app-whiteboard',
   imports: [NgWhiteboardComponent, CommonModule, FormsModule, HttpClientModule],
