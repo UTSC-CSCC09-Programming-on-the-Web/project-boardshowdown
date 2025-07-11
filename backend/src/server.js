b4c4cac3998e1f649559172d94382cfeb5f580a7
IlyaKIS1 <ilyuhakostin@gmail.com>
2025-07-11 14:13:49 -0400

    size extension fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -59,6 +59,7 @@ app.options('*', cors({
 
 app.use(bodyParser.json({ limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 /*
 globalThis.fetch = fetch;
 globalThis.Headers = Headers;
