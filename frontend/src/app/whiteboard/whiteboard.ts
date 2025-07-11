12e7e6a42e87759592f13817b75ace5013dc8ae7
IlyaKIS1 <ilyuhakostin@gmail.com>
2025-07-11 18:06:51 -0400

    trigger build

--- frontend/src/app/whiteboard/whiteboard.ts
+++ frontend/src/app/whiteboard/whiteboard.ts
@@ -19,7 +19,7 @@ import { environment } from '../../environments/environment';
 import { WebsocketProvider } from 'y-websocket';
 import { QuestionService, Question, CheckSolutionResult } from '../services/question.service';
 
-//deploy 4x
+//deploy 5x
 @Component({
   selector: 'app-whiteboard',
   imports: [NgWhiteboardComponent, CommonModule, FormsModule, HttpClientModule],
