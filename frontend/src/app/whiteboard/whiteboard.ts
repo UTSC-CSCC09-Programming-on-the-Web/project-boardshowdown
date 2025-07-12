f21e252b5626247ce11acc378bcb03f1d8bd3b83
IlyaKIS1 <ilyuhakostin@gmail.com>
2025-07-12 00:48:34 -0400

    changed ports for yjs

--- frontend/src/app/whiteboard/whiteboard.ts
+++ frontend/src/app/whiteboard/whiteboard.ts
@@ -19,7 +19,7 @@ import { environment } from '../../environments/environment';
 import { WebsocketProvider } from 'y-websocket';
 import { QuestionService, Question, CheckSolutionResult } from '../services/question.service';
 
-//deploy 6x
+//deploy 7x
 @Component({
   selector: 'app-whiteboard',
   imports: [NgWhiteboardComponent, CommonModule, FormsModule, HttpClientModule],
