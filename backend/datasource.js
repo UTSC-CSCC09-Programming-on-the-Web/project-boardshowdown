db19e860ad079e2ca0740389a726392d2943adab
IlyaKIS1 <ilyuhakostin@gmail.com>
2025-07-22 17:47:20 -0400

    fixed postgres db

--- backend/datasource.js
+++ backend/datasource.js
@@ -1,14 +1,12 @@
 import pg from "pg";
 //deploy 10x
 
-const POSTGRES_DB = process.env.POSTGRES_DB || 'postgres';
 
 export const client = new pg.Client({
   host: process.env.POSTGRES_HOST || "localhost",
   user: process.env.POSTGRES_USER || "postgres",
   password: process.env.POSTGRES_PASSWORD || "postgres",
   port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432,
-  database: POSTGRES_DB,
 });
 
 client.on('error', (err) => {
