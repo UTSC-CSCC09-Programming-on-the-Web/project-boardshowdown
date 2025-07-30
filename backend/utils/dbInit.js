fd6d196a8fcead7c7442c87dae81edc60a2bf120
arinaazmi <arina.cat2003@gmail.com>
2025-07-30 14:34:13 -0400

    questions solved

--- backend/utils/dbInit.js
+++ backend/utils/dbInit.js
@@ -66,12 +66,17 @@ export async function initializeDatabase() {
 async function insertSampleQuestions() {
   const sampleQuestions = [
     {
-      question: "Suppose you roll a fair 6‑sided die until you've seen all 6 face...n odd numbered face until you have seen all even numbered faces?",
+      question: "What is 2 + 2?",
+      solution: 4,
+      difficulty: 100
+    },
+    {
+      question: "Suppose you roll a fair 6-sided die until you've seen all 6 faces. What is the probability you won't see an odd numbered face until you have seen all even numbered faces?",
       solution: 0.05,
       difficulty: 100
     },
     {
-      question: "A baby is learning to walk with the assistance of its living r...ime, what proportion of the time would the baby be at the couch?",
+      question: "A baby is learning to walk with the assistance of its living room furniture. If the baby is at the couch 33% of the time, what proportion of the time would the baby be at the couch?",
       solution: 0.333,
       difficulty: 100
     },
@@ -81,7 +86,7 @@ async function insertSampleQuestions() {
       difficulty: 100
     },
     {
-      question: "Assume you are given a stick that is 1 meter in length. Rando...s the probability that the smallest piece is at most 0.2 meter?",
+      question: "Assume you are given a stick that is 1 meter in length. Randomly select 2 points on the stick and break it at these 2 points to get 3 pieces. What's the probability that the smallest piece is at most 0.2 meter?",
       solution: 0.84,
       difficulty: 100
     },
@@ -101,27 +106,27 @@ async function insertSampleQuestions() {
       difficulty: 100
     },
     {
-      question: "Alice and Bob each have a coin and flip it until they get a h...ce, what is the expected number of times Alice flipped her coin?",
+      question: "Alice and Bob each have a coin and flip it until they get a heads. If Bob flipped his coin more times than Alice, what is the expected number of times Alice flipped her coin?",
       solution: 1.333,
       difficulty: 100
     },
     {
-      question: "An electronic safe has a three-digit code. You are given two ...fy these two requirements?",
+      question: "An electronic safe has a three-digit code. You are given two hints regarding the code to the safe: None of the digits are zero, and the sum of the digits does not exceed ten. How many possible three-digit entries satisfy these two requirements?",
       solution: 120,
       difficulty: 100
     },
     {
-      question: "There are five boxes: one with $150, the rest are empty. At a... that makes the game fair?",
+      question: "There are five boxes: one with $150, the rest are empty. At any time, you may pay X dollars to open one of the boxes and keep the contents. Assuming you continue paying until you find the box with the money, what is the value X (in dollars) that makes the game fair?",
       solution: 50,
       difficulty: 100
     },
     {
-      question: "You have a pet cat that has a sly tendency to leave your hou...our neighbor says it did so?",
+      question: "You have a pet cat that has a sly tendency to leave your house while you are away. Every day while you are out, you call your neighbor to ask them whether your cat has left your house. Your neighbor always responds with a yes or a no, but sometimes they lie, saying that your cat left when it has not, or vice versa. Let's say that the probability that your cat leaves the house is 0.001 and the probability that your neighbor lies is 0.1 (independent of whether your cat actually left the house or not). What is the probability that your cat left the house given that your neighbor says it did so?",
       solution: 0.009,
       difficulty: 100
     },
     {
-      question: "You are playing a 2D game where your character is trapped in...ects at least one power-up?",
+      question: "You are playing a 2D game where your character is trapped in a 6 × 6 grid. Your character starts at (1, 1), and can only move down and right. There are two power-ups located at (2, 3) and (4, 6). How many possible paths can your character take to get to (6 ,6), such that it collects at least one power-up?",
       solution: 131,
       difficulty: 100
     },
@@ -131,15 +136,16 @@ async function insertSampleQuestions() {
       difficulty: 100
     },
     {
-      question: "You roll a fair six-sided and sum the outcomes until you rea...hat is the expected number of times you expect to roll the die.",
+      question: "You roll a fair six-sided and sum the outcomes until you reach a multiple of 6. What is the expected number of times you expect to roll the die.",
       solution: 6,
       difficulty: 100
     },
     {
-      question: "Paul flips a fair coin until he obtains two consecutive head...nd the probability Paul flips the coin an even number of times.",
+      question: "Paul flips a fair coin until he obtains two consecutive heads or tails for the first time. Find the probability Paul flips the coin an even number of times.",
       solution: 0.667,
       difficulty: 100
-    }
+    } 
+    
   ];
 
   for (const q of sampleQuestions) {
