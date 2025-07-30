b06978466594e6c0131e9905f3cbd1ea2547ee44
IlyaKIS1 <ilyuhakostin@gmail.com>
2025-07-30 01:26:17 -0400

    fixed the parser warning

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ app.use(cors({
 
 app.use(bodyParser.json({ limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
-app.use(express.urlencoded({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 
 
 
